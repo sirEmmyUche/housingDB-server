a8efdd7a478450163507402095694202d8f7e552
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2023-04-16 21:42:50 +0100

    initial commit

--- routes/registerhouse.js
+++ routes/registerhouse.js
@@ -73,19 +73,17 @@ const checkFileType = (file, cb)=>{
 
 const upload = multer({
   storage: multerStorage,
-  fileFilter: checkFileType,
+  // fileFilter: checkFileType
 })
 
 
 const cpUpload = upload.fields([{name:"houseImage", maxCount: 1},{name:"proofOfOwnership", maxCount: 1}])
 router.post('/api/uploadFile', cpUpload , function(req, res, next){
 try{
-   // req.files is array of `photos` files
-  // req.body will contain the text fields, if there were any
   const {nameOfOwner,houseNumber,street,LGA,state,} = req.body;
   console.log(req.files);
   console.log(req.files);
-  if (req.files === ""){
+  if (!req.files){
    return  res.status(404).json("Please upload a file")
   }else{
     const newHouseRegistration = new RegisterHouse({
@@ -127,120 +125,4 @@ const multerFilter = (req, file, cb) => {
   }
 };
 
-
-
-
-  const path = require('path');
-    const multer = require('multer');
-    const storage = multer.diskStorage({
-         destination: (req, file, cb) => {
-           if (file.fieldname === "profile") {
-               cb(null, './uploads/profiles/')
-           }
-           else if (file.fieldname === "natid") {
-               cb(null, './uploads/ids/');
-           }
-           else if (file.fieldname === "certificate") {
-               cb(null, './uploads/certificates/')
-           }
-        },
-        filename:(req,file,cb)=>{
-            if (file.fieldname === "profile") {
-                cb(null, file.fieldname+Date.now()+path.extname(file.originalname));
-            }
-          else if (file.fieldname === "natid") {
-            cb(null, file.fieldname+Date.now()+path.extname(file.originalname));
-          }
-          else if (file.fieldname === "certificate") {
-            cb(null, file.fieldname+Date.now()+path.extname(file.originalname));
-          }
-        }
-    });
-    const upload = multer({
-        storage: storage,
-        limits: {
-            fileSize: 1024 * 1024 * 10
-        },
-        fileFilter: (req, file, cb) => {
-            checkFileType(file, cb);
-        }
-    }).fields(
-        [
-            {
-                name:'profile',
-                maxCount:1
-            },
-            {
-                name: 'natid', maxCount:1
-            },
-            {
-                name: 'certificate', maxCount:1
-            }
-        ]
-    );
-    
-    function checkFileType(file, cb) {
-        if (file.fieldname === "certificate") {
-            if (
-                file.mimetype === 'application/pdf' ||
-                file.mimetype === 'application/msword' ||
-                file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-              ) { // check file type to be pdf, doc, or docx
-                  cb(null, true);
-              } else {
-                  cb(null, false); // else fails
-              }
-        }
-        else if (file.fieldname === "natid" || file.fieldname === "profile") {
-            if (
-                file.mimetype === 'image/png' ||
-                file.mimetype === 'image/jpg' ||
-                file.mimetype === 'image/jpeg'||
-                fiel.mimetype==='image/gif'
-              ) { // check file type to be png, jpeg, or jpg
-                cb(null, true);
-              } else {
-                cb(null, false); // else fails
-              }
-            }
-        }
-    //at the save function 
-
- upload(req, res, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (req.file == "undefined") {
-                console.log("No image selected!")
-            } else {
-                let datecreated = new Date();
-                let fullnames = req.body.firstname + ' ' + req.body.lastname;
-                let formatedphone = '';
-                let phone = req.body.personalphone;
-                if (phone.charAt(0) == '0') {
-                    formatedphone = '+254' + phone.substring(1);
-                } else if ((phone.charAt(0) == '+') && (phone.length > 12 || phone.length <= 15)) {
-                    formatedphone = phone
-                }
-                let teachers = {
-                    "teacherid": teacherid,
-                    "schoolcode": req.body.schoolcode,
-                    "fullnames": fullnames,
-                    "email": req.body.email,
-                    "dateofbirth": req.body.dateofbirth,
-                    "nationalid": req.body.nationalid,
-                    "personalphone": formatedphone,
-                    "profile": req.files.profile[0].path,
-                    "natid": req.files.natid[0].path,
-                    "certificate":req.files.certificate[0].path
-                }
-                connection.query('INSERT INTO teachers SET ?', teachers, (error, results, fields) => {`enter code here`
-                    if (error) {
-                        res.json({
-                            status: false,
-                            message: 'there are some error with query'
-                        })
-                        console.log(error);
-                    } else {console.log("Saved successfully");
-}
 */
