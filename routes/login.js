bd411d29df62bea60077e1139824b918879ff091
Uche Osuchukwu <95975151+sirEmmyUche@users.noreply.github.com>
2023-09-11 16:31:22 +0100

    adding the verify token route

--- routes/login.js
+++ routes/login.js
@@ -50,6 +50,49 @@ router.post("/login", async(req, res)=>{
   }
 
 })
- 
+
+
+router.get("/verifyToken", async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      // If the Authorization header is missing, return an error
+      return res.status(401).json({ message: "Authorization header missing" });
+    }
+
+    // Split the Authorization header into two parts: "Bearer" and the token
+    const [bearer, token] = authHeader.split(" ");
+
+    if (bearer !== "Bearer" || !token) {
+      // If the header doesn't start with "Bearer" or the token is missing, return an error
+      return res.status(401).json({ message: "Invalid Authorization header" });
+    }
+
+    // Now, you can verify the token
+    const isVerified = jwt.verify(token, process.env.JWT_SECRET, function (
+      err,
+      decoded
+    ) {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ message: "Session Expired, Please Login" });
+      }
+
+      const user = {
+        username: decoded.username,
+        userId: decoded.userId,
+      };
+      return res.status(200).json(user);
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json("Internal Server Error");
+  }
+});
 
 module.exports = router;
+
+
